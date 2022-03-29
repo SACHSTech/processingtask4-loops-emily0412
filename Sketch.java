1de31ff97b514185b34bcc0355b94b4012ff4cca
Emily Ma <99974145+emily0412@users.noreply.github.com>
2022-03-29 21:05:04 +0000

    added comments

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,63 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
+	
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Quadrant 1
+    //prints vertical lines
+    for (int lineOne = (width/20); lineOne <= (width/(20/9)); lineOne += (width/20)) {
+      stroke (0);
+      line(lineOne, 0, lineOne, width/2);
+    }
+    // prints horizontal lines
+    for (int lineOne = (width/20); lineOne <= (width/(20/9)); lineOne += (width/20)) {
+      stroke (0);
+      line(0, lineOne, width/2, lineOne);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Quadrant 2
+    for (int circleX = (int)(width/(20.0/11.0)); circleX <= width; circleX += (width/10)) {
+      for(int circleY = (width/20); circleY <= (int)(width/(20.0/9.0)); circleY += (width/10)) {
+        fill (212, 15, 125);
+        stroke (0);
+        ellipse(circleX, circleY, (width/20), (width/20));
+      }
+    }
+    
+    // Quadrant 3
+    float colourAdjust = (float)255/(width/2); // multiply the gradient by this number to get 255 colours within the quadrant
+
+    for (int gradient = 0; gradient <= (width/2); gradient++) {
+      stroke((float)(gradient * colourAdjust), (float)(gradient * colourAdjust), (float)(gradient * colourAdjust)); // colour of line
+      line(gradient, width/2, gradient, height); 
+    }
+
+    // Quadrant 4
+    
+    // petals
+    pushMatrix(); // controls translate()
+    fill(220, 93, 52); // petal colour
+    stroke(0);
+    translate ((float)(width * 0.75), (float)(height * 0.75)); // changes the origin from (0,0) to (width*0.75, height*0.75)
+    for (int petals = 0; petals < 8; petals++) {
+      rotate(PI/4);
+      ellipse(width/70, height/10, width/20, height/6);
+    }
+    popMatrix(); // resets translate ()
+
+    // middle circle
+    noStroke();
+    fill(38, 108, 45);
+    ellipse(width*((float)(0.75)), width*((float)(0.75)), width/10, width/10);
   }
   
-  // define other methods down here.
 }
